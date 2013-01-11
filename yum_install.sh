bc6d71974499ca6a2349ec2a19b018f8d38a297c
kjirou <kjirou.web@gmail.com>
2013-01-11 12:20:37 +0900

    .

--- yum_install.sh
+++ yum_install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#
+# Install script for CentOS 5 or 6
+#
+
 #yum update
 
 yum install zsh
@@ -16,6 +20,9 @@ yum install libevent-devel
 yum install libxslt-devel
 # For Ruby/Passenger
 yum install libyaml-devel
+yum install rpm-devel
+yum install rpmdevtools
+yum install rpm-build
 yum install zlib-devel
 yum install readline-devel
 yum install curl-devel
@@ -26,16 +33,25 @@ yum install freetype-devel
 yum install bzip2-devel
 yum install sqlite3
 yum install sqlite3-devel
+yum install tk-devel
+yum install tcl-devel
+yum install db4-devel
+yum install gdbm-devel
+yum install mysql-devel
+yum install python-devel
+yum install gcc
 yum install gcc-c++
+yum install compat-gcc-34-c++
 yum install gd-devel
 yum install gd-progs
 # For Ruby/Passenger
 yum install ImageMagick-devel
+# For Python/Pillow
+yum install lcms-devel
 yum install subversion
+yum install git
 yum install tree
 yum install nmap
-yum install python-devel
-yum install mysql-devel
 yum install java-1.7.0-openjdk
 yum install java-1.7.0-openjdk-devel
 # For Selenium
